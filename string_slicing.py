a8abd9942605da5c1359425d7d06c0f6655bcf45
anil-kumar981 <yadavanilkumar8113@gmail.com>
2026-05-21 11:41:08 +0530

    Slicing In python

---
+++ string_slicing.py
@@ -0,0 +1,14 @@
+name = "Anil kumar yadav"
+print(name[0])  # A
+print(name[0:4]) # run from zero to three # Anil
+print(name[5:10]) # kumar
+print(name[11:]) # yadav
+print(name[:5]) # Anil
+print(name[:]) # Anil kumar yadav   
+
+print(name[0:]) # Anil kumar yadav
+print(name[::-1]) # davay ramuk lina(reverse)
+print(len(name)) # 19
+print(name[0:19:2]) # A lkmrydv (run from zero to 18 with step 2)
+print(name[::2]) # A lkmrydv (run from zero to 18 with step 2)
+print(name[1:19:2]) # nilkuyad (run from one to 18 with step 2)
