547bb47812d32e9dff1258a493ac77f95087c3da
anil-kumar981 <yadavanilkumar8113@gmail.com>
2026-05-23 16:28:28 +0530

    short_hand_conditionals and enumrated function

---
+++ enumrated_function.py
@@ -0,0 +1,5 @@
+a = [1, 2, 3, 4, 5]
+
+# Using enumerate to get index and value
+for index, value in enumerate(a):
+    print(f"Index: {index}, Value: {value}")
---
+++ short_hand_if_elif.py
@@ -0,0 +1,4 @@
+a = 13
+b = 13
+c= 15 if a > b else 20 
+print(c)  # Output: 20
